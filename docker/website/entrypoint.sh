0515a34cdd21fb1c41842164b79d7b00561c4990
Santiago Marulanda <santiagomm1997@gmail.com>
2024-07-01 21:18:46 +0200

    [TIAG-10] Add support for website project

--- docker/website/entrypoint.sh
+++ docker/website/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -f "Cargo.toml" ]; then
     echo "[INFO] skip clonning process to /var/www/app"
 else
     echo "[INFO] clone the application to /var/ww/app"
-    git clone https://github.com/tiagocodecom/back-office.git . 
+    git clone https://github.com/tiagocodecom/website.git . 
 fi
 
-cargo watch -x run
+trunk serve --port 3000 --address 0.0.0.0
