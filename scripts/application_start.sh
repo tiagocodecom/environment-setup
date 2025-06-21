f6a5a08fa41d7c3e755d9f64a34823fb6fda2a9f
Santiago Marulanda <santiagomm1997@gmail.com>
2025-06-21 19:20:57 +0200

    Remove unnecessary backoffice volume

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -15,4 +15,4 @@ echo "$GITHUB_PAT" | docker login ghcr.io -u "smarulanda97" --password-stdin
 docker-compose \
   -f compose.yml \
   -f compose.production.yml \
-  up -d --build --pull always --force-recreate traefik nginx backoffice website redis
+  up -d --build --pull always --force-recreate
