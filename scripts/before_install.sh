60c9a70c6573e0409e56d58c01ac5e07e9682def
Santiago Marulanda <santiagomm1997@gmail.com>
2026-01-20 23:44:19 +0100

    Downgrade docker compose version to fix bug with Buildx

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -13,6 +13,6 @@ sudo usermod -aG docker ec2-user
 newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.39.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
